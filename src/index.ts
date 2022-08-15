9ad6554d6c23551bc87177b3e1f25e156864230a
Tangjj1996 <awesometangji@gmail.com>
2022-08-15 13:34:46 +0800

    feat: prepare devdependencies and dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-const a = 1;
+import express from "express";
+import config from "config";
+
+const port = config.get<number>("port");
+
+const app = express();
+
+app.listen(port, () => {
+  console.log();
+});
