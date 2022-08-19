f8f4d5184a72131c18c51fbd72e6643e8a77055c
Tangjj1996 <awesometangji@gmail.com>
2022-08-19 11:50:31 +0800

    feat: add web server config

---
+++ packages/server/rollup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "rollup";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+
+export default defineConfig({
+  input: "src/index.ts",
+  output: {
+    dir: "dist",
+    format: "cjs",
+  },
+  plugins: [
+    nodeResolve({ preferBuiltins: false }),
+    commonjs(),
+    json(),
+    typescript({ compilerOptions: { outDir: "dist" }, module: "esnext" }),
+  ],
+});
